2c6aa9931bb66dd163e3ea3764b3df3a888d6cd0
Cardon Burnham <ayliasthecoder@protonmail.com>
2021-12-11 19:11:28 -0700

    Initial Commit (fixed like 2.7 trillion bugs)

---
+++ src/me/aylias/parrot/lifesteal/Events.java
@@ -0,0 +1,192 @@
+package me.aylias.parrot.lifesteal;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.BanList.Type;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Events implements Listener {
+    List<String> deadPlayers;
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity();
+        Player killer = e.getEntity().getKiller();
+        AttributeInstance attrDead = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        double baseDead = attrDead.getBaseValue();
+        AttributeInstance attrKiller = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        double baseKiller = attrKiller.getBaseValue();
+        if (baseDead > 2.0D) {
+            attrDead.setBaseValue(baseDead - 2.0D);
+            if (killer != null && baseKiller <= 38.0D) {
+                attrKiller.setBaseValue(baseKiller + 2.0D);
+            }
+        } else {
+            this.deadPlayers = LifeSteal.instance.getConfig().getStringList("Dead");
+            this.deadPlayers.add(player.getUniqueId().toString());
+            LifeSteal.getInstance().getConfig().set("Dead", this.deadPlayers);
+            LifeSteal.getInstance().saveConfig();
+            if (killer != null) {
+                player.getInventory().clear();
+                Bukkit.getBanList(Type.NAME).addBan(player.getName(), ChatColor.RED + "You died because of " + ChatColor.GOLD + killer.getName() + ChatColor.RED + ".", (Date)null, player.getName());
+                ChatColor var10001 = ChatColor.RED;
+                player.kickPlayer(var10001 + "You died because of " + ChatColor.GOLD + killer.getName() + ChatColor.RED + ".");
+                if (killer.getMaxHealth() <= 38.0D) {
+                    killer.setMaxHealth(killer.getMaxHealth() + 2.0D);
+                }
+            } else {
+                Bukkit.getBanList(Type.NAME).addBan(player.getName(), ChatColor.RED + "You died.", (Date)null, player.getName());
+                player.kickPlayer(ChatColor.RED + "You died.");
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        List<String> revivedList = LifeSteal.getInstance().getConfig().getStringList("Revived");
+        String reviver = null;
+        String wholeString = null;
+        Iterator var6 = revivedList.iterator();
+
+        while(var6.hasNext()) {
+            String uuid = (String)var6.next();
+            String[] uuids = uuid.split("%");
+            if (UUID.fromString(uuids[0]).equals(player.getUniqueId())) {
+                reviver = Bukkit.getOfflinePlayer(UUID.fromString(uuids[1])).getName();
+                wholeString = uuid;
+                break;
+            }
+        }
+
+        if (reviver != null) {
+            player.setMaxHealth(6.0D);
+            player.sendMessage(ChatColor.GREEN + "You were revived by " + reviver + "!");
+            revivedList.remove(wholeString);
+            LifeSteal.getInstance().getConfig().set("Revived", revivedList);
+        }
+
+        LifeSteal.getInstance().saveConfig();
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Action act = e.getAction();
+        Player player = e.getPlayer();
+        Inventory playerInv = player.getInventory();
+        if (e.getItem() != null && (act.equals(Action.RIGHT_CLICK_AIR) || act.equals(Action.RIGHT_CLICK_BLOCK))) {
+            if (e.getItem().isSimilar(LifeSteal.getInstance().beaconOfLife)) {
+                new GUI(player, 1);
+            } else if (e.getItem().isSimilar(LifeSteal.getInstance().heart) && player.getMaxHealth() <= 38.0D) {
+                for(int i = 0; i < playerInv.getSize(); ++i) {
+                    if (playerInv.getItem(i) != null && playerInv.getItem(i).isSimilar(LifeSteal.getInstance().heart)) {
+                        playerInv.getItem(i).setAmount(playerInv.getItem(i).getAmount() - 1);
+                        break;
+                    }
+                }
+
+                AttributeInstance attr = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+                attr.setBaseValue(attr.getBaseValue() + 2.0D);
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Material blockType = e.getBlock().getType();
+        String displayName = e.getItemInHand().getItemMeta().getDisplayName();
+        if (blockType.equals(Material.BEACON) && e.getItemInHand().getItemMeta() != null && displayName.equals(ChatColor.LIGHT_PURPLE + "Beacon Of Life")) {
+            e.setCancelled(true);
+        }
+
+    }
+
+    @EventHandler
+    public void playerCraft(CraftItemEvent e) {
+        ItemStack[] var2 = e.getInventory().getContents();
+        int var3 = var2.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            ItemStack content = var2[var4];
+            if (content != null && content.getType().equals(Material.CARROT_ON_A_STICK)) {
+                ItemMeta meta = content.getItemMeta();
+                if (!meta.hasCustomModelData()) {
+                    e.setCancelled(true);
+                }
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (LifeSteal.getInstance().gui != null) {
+            HumanEntity var3 = e.getWhoClicked();
+            if (var3 instanceof Player) {
+                Player player = (Player)var3;
+                Inventory playerInv = player.getInventory();
+                ItemStack item = e.getCurrentItem();
+                Inventory inv = e.getInventory();
+                int slot = e.getRawSlot();
+                if (item != null) {
+                    item.getType();
+                    if (inv.equals(LifeSteal.getInstance().gui)) {
+                        e.setCancelled(true);
+                        int page = Integer.parseInt(inv.getItem(36).getItemMeta().getLocalizedName());
+                        if (slot == 36 && item.getType().equals(Material.LIME_WOOL)) {
+                            new GUI(player, page - 1);
+                        } else if (slot == 44 && item.getType().equals(Material.LIME_WOOL)) {
+                            new GUI(player, page + 1);
+                        } else if (item.getType().equals(Material.PLAYER_HEAD) || item.getType().equals(Material.LEGACY_SKULL_ITEM)) {
+                            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(ChatColor.stripColor(item.getItemMeta().getDisplayName()));
+                            List<String> deadList = LifeSteal.getInstance().getConfig().getStringList("Dead");
+                            deadList.remove(offlinePlayer.getUniqueId().toString());
+                            LifeSteal.getInstance().getConfig().set("Dead", deadList);
+                            List<String> revivedList = LifeSteal.getInstance().getConfig().getStringList("Revived");
+                            String var10001 = offlinePlayer.getUniqueId().toString();
+                            revivedList.add(var10001 + "%" + player.getUniqueId().toString());
+                            LifeSteal.getInstance().getConfig().set("Revived", revivedList);
+                            LifeSteal.getInstance().saveConfig();
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pardon " + offlinePlayer.getName());
+
+                            for(int i = 0; i < playerInv.getSize(); ++i) {
+                                if (playerInv.getItem(i) != null && playerInv.getItem(i).isSimilar(LifeSteal.getInstance().beaconOfLife)) {
+                                    playerInv.getItem(i).setAmount(playerInv.getItem(i).getAmount() - 1);
+                                    break;
+                                }
+                            }
+
+                            player.closeInventory();
+                            ChatColor var13 = ChatColor.GREEN;
+                            player.sendMessage(var13 + "Successsfully revived " + offlinePlayer.getName() + "!");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ src/me/aylias/parrot/lifesteal/LifeSteal.java
@@ -0,0 +1,92 @@
+/* Decompiler 36ms, total 266ms, lines 93 */
+package me.aylias.parrot.lifesteal;
+
+import java.util.Arrays;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LifeSteal extends JavaPlugin implements Listener {
+   public static LifeSteal instance;
+   public ItemStack beaconOfLife;
+   public ItemStack heart;
+   public ItemStack heartFragment;
+   Inventory gui;
+
+   public void onEnable() {
+      instance = this;
+      this.getConfig().options().copyDefaults();
+      this.saveDefaultConfig();
+      this.beaconOfLife = new ItemStack(Material.BEACON);
+      ItemMeta beaconOfLifeMeta = this.beaconOfLife.getItemMeta();
+      beaconOfLifeMeta.setDisplayName(ChatColor.LIGHT_PURPLE + "Beacon Of Life");
+      beaconOfLifeMeta.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "Right-Click this to bring somone back to life!"));
+      this.beaconOfLife.setItemMeta(beaconOfLifeMeta);
+      this.heart = new ItemStack(Material.FIREWORK_STAR);
+      ItemMeta heartMeta = this.heart.getItemMeta();
+      heartMeta.setDisplayName(ChatColor.RED + "Heart");
+      heartMeta.setLore(Arrays.asList(ChatColor.LIGHT_PURPLE + "Use 4 of these to craft the Beacon Of Life!"));
+      this.heart.setItemMeta(heartMeta);
+      this.heartFragment = new ItemStack(Material.CARROT_ON_A_STICK);
+      ItemMeta heartFragmentMeta = this.heartFragment.getItemMeta();
+      heartFragmentMeta.setDisplayName(ChatColor.RED + "Heart Fragment");
+      heartFragmentMeta.setLore(Arrays.asList(ChatColor.LIGHT_PURPLE + "Use 4 of these to craft a Heart!"));
+      this.heartFragment.setItemMeta(heartFragmentMeta);
+      new Recipes(this);
+      Bukkit.getPluginManager().registerEvents(new Events(), this);
+   }
+
+   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+      if (label.equalsIgnoreCase("withdraw") && sender instanceof Player player && args.length == 1) {
+         boolean var6 = false;
+
+         int hearts;
+         try {
+            hearts = Integer.parseInt(args[0]);
+         } catch (NumberFormatException var10) {
+            player.sendMessage(ChatColor.RED + "Please put in the number of hearts you have, and make sure it's an integer!");
+            return false;
+         }
+
+         if (hearts > 0) {
+            int playerHearts = (int)player.getMaxHealth() - (hearts * 2);
+            if (playerHearts > 0) {
+               player.setMaxHealth(playerHearts);
+               int i;
+               if (player.getInventory().firstEmpty() != -1) {
+                  for(i = 0; i < hearts; ++i) {
+                     player.getInventory().addItem(getInstance().heart);
+                  }
+               } else {
+                  for(i = 0; i < hearts; ++i) {
+                     player.getWorld().dropItemNaturally(player.getLocation(), getInstance().heart);
+                  }
+               }
+
+               player.sendMessage(ChatColor.GOLD + "Successfully withdrawed hearts!");
+            } else {
+               player.sendMessage(ChatColor.RED + "You can't withdraw more or the same amount of hearts than you currently have!");
+            }
+         } else if (hearts == 0) {
+            player.sendMessage(ChatColor.GOLD + "Whats the point of withdrawing 0 hearts!");
+         } else {
+            ChatColor var10001 = ChatColor.GOLD;
+            player.sendMessage(var10001 + "" + player.getMaxHealth() + " - " + hearts + " = " + (player.getMaxHealth() - (double)hearts) + ", and that's greater than your original hearts! You think imma let you add hearts on a /withdraw command!");
+         }
+      }
+
+      return true;
+   }
+
+   public static LifeSteal getInstance() {
+      return instance;
+   }
+}
